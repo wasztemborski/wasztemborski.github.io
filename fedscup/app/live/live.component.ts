2ecb42090d08b4ca4c5e865b897f6a48bbae6e0b
whai <whai@gft.com>
2026-02-09 12:29:22 +0100

    removed fedscup

--- fedscup/app/live/live.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  moduleId: module.id,
-  selector: 'fedscup-live',
-  templateUrl: './live.component.html'
-})
-export class LiveComponent { }
--- fedscup/app/scores/scores.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    moduleId: module.id,
-    selector: 'fedscup-scores',
-    templateUrl: './scores.component.html'
-})
-export class ScoresComponent { }
